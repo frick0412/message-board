b02be85b6719117964cd01dfb7603300b0d64478
frick0412 <yuu72ki@yahoo.co.jp>
2016-03-27 14:30:12 +0000

    challenge2 files

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,6 +25,7 @@ class MessagesController < ApplicationController
   end
   
   def create
+    
     @message = Message.new(message_params)
     if @message.save
       redirect_to root_path , notice: 'メッセージを保存しました'
@@ -39,7 +40,7 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :age, :body)
   end
 
  def set_message
