84ca040d49e2f03e44ac6857fb794cf5ea195705
frick0412 <yuu72ki@yahoo.co.jp>
2016-03-28 11:40:45 +0000

    message.rb commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   root 'messages#index'
   resources :messages , except: [:index, :new]
   # The priority is based upon order of creation: first created -> highest priority.
